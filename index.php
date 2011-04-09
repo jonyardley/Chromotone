02d7912a93d0d259b344b9c54c731bd5c498d519
Jon Yardley <jon.c.yardley@googlemail.com>
2011-04-09 20:03:11 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="">
+<head>
+	<meta charset="utf-8">
+	<title>Chromotone | Explore the Universe with sound</title>
+	<meta name="description" content="" />
+	<meta name="keywords" content="" />
+	<meta name="robots" content="" />
+	<link href="css/screen.css" media="screen" rel="stylesheet" type="text/css" />
+	<script type="text/javascript" src="js/jquery-1.5.2.min.js"></script>
+</head>
+<body>
+	
+</body>
+</html>
